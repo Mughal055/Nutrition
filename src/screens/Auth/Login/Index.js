b5d294877ae988836c790fa2cf2feeab082fad30
Mughal Queen <mughalqueen055@gmail.com>
2025-12-01 18:56:27 +0500

    Error

--- src/screens/Auth/Login/Index.js
+++ src/screens/Auth/Login/Index.js
@@ -5,7 +5,7 @@ import { a } from '../../../style/style';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Buttoon from '../../../components/Button';
-// import CustomHeader from '../../../components/Header'
+import CustomHeader from '../../../components/Header'
 import { useNavigation } from '@react-navigation/native';
 
 const Login = () => {
@@ -14,7 +14,7 @@ const Login = () => {
     <View style={{flex:1}}>
       <View style={{flex:1}}>
             <Ionicons name="arrow-back-outline" size={24} color='#000' style={{marginTop:22,marginLeft:22}} />
-            {/* <CustomHeader title='Login to your account' desc='Oh, were meeting again, hurry upand log in to see the new posts!'/> */}
+            <CustomHeader title='Login to your account' desc='Oh, were meeting again, hurry upand log in to see the new posts!'/>
                 
     
     <View style={{flex:1}}>
