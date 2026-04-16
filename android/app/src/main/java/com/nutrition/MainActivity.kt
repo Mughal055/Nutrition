f83236c7c045ec52a85b4a5d15e946dd4359395a
AmirMirza9433 <amirmirza9433@gmail.com>
2026-04-16 14:57:45 +0500

    code push

--- android/app/src/main/java/com/nutrition/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.nutrition // Jo build.gradle mein hai wahi yahan likhein
-import com.facebook.react.ReactActivity
-
-class MainActivity : ReactActivity() {
-    override fun getMainComponentName(): String = "Nutrition" 
-}
--- src/assets/fonts/Fonts.js
+++
@@ -1,8 +0,0 @@
-export default {
-    PoppinsRegular:'Poppins-Regular',
-    PoppinsBlack:'Poppins-Black',
-    PoppinsBold:'Poppins-Bold',
-    PoppinsExtraBold:'Poppins-ExtraBold',
-    PoppinsMedium:'Poppins-Medium',
-    PoppinsSemiBold:'Poppins-SemiBold',
-}
